ce61ce613d20a8dd255ee4c3a7d39bfdc416aa33
Muzdalifa <a.muzdalifa27@gmail.com>
2021-08-31 21:11:08 +0200

    installing Microsoft.EntityFrameworkCore using nuget package

---
+++ LexiconAssignment7_MVCDataManagement/Data/DatabasePeopleRepo.cs
@@ -0,0 +1,42 @@
+﻿using LexiconAssignment7_MVCDataManagement.Models;
+using LexiconAssignment7_MVCDataManagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconAssignment7_MVCDataManagement.Data
+{
+    public class DatabasePeopleRepo : IPeopleRepo
+    {
+        private readonly IPeopleRepo _peopleRepo;
+        public DatabasePeopleRepo()
+        {
+            _peopleRepo = peopleRepo;
+        }
+        public Person Create(CreatePersonViewModel person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Update(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
