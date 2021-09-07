4f4280d001153c21f8ec6648c15b0d843d912060
Muzdalifa <a.muzdalifa27@gmail.com>
2021-09-07 11:56:02 +0200

    Edit createPersonViewModel to allow swedish characters

--- LexiconAssignment7_MVCDataManagement/ViewModels/CreatePersonViewModel.cs
+++ LexiconAssignment7_MVCDataManagement/ViewModels/CreatePersonViewModel.cs
@@ -7,7 +7,9 @@ namespace LexiconAssignment7_MVCDataManagement.ViewModels
     public class CreatePersonViewModel
     {
         [Required(ErrorMessage = "You need to fill out name field!")]
-        [RegularExpression(@"[A-z]*", ErrorMessage = "Use only alphabets.")]
+        //[RegularExpression(@"[A-z]*", ErrorMessage = "Use only alphabets.")]
+        [RegularExpression(@"[A-zåäö]*", ErrorMessage = "Use only alphabets.")]
+        
         [MinLength(2),MaxLength(50)]
         //[StringLength(50)]
         public string Name { set; get; }
